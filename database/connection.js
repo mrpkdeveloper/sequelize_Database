aab37436989093af22510c210e2b9bb90791319e
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-07-09 13:43:55 +0530

    connection and models created

---
+++ database/connection.js
@@ -0,0 +1,12 @@
+const sequelize = require('sequelize')
+
+const db = new sequelize('batchdb', 'batchuser', 'batchpass', {
+    dialect: "mysql",
+    host: "localhost"
+})
+
+db.authenticate()
+    .then(() => { console.log("databse connected successfully") })
+    .catch((e) => { console.log("e") })
+
+module.exports = { db }    
---
+++ database/models.js
@@ -0,0 +1,69 @@
+const { db } = require("./connection")
+const { DataTypes } = require('sequelize')
+
+const course = db.define('course', {
+    id: {
+        type: DataTypes.STRING(2),
+        primaryKey: true
+    },
+    name: DataTypes.STRING(20)
+})
+
+
+const teacher = db.define('teacher', {
+    name: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    }
+})
+
+const center = db.define('center', {
+    id: {
+        type: DataTypes.STRING(2),
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    },
+    city: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    }
+
+})
+
+const season = db.define('season', {
+    id: {
+        type: DataTypes.STRING(2),
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    }
+
+})
+
+const batch = db.define('batch', {
+    code: {
+        type: DataTypes.STRING(8),
+        primaryKey: true
+    },
+    year: DataTypes.INTEGER(4),
+    start: DataTypes.DATE,
+    end: DataTypes.DATE
+
+})
+
+
+//association
+batch.belongsTo(course)
+batch.belongsTo(center)
+batch.belongsTo(season)
+
+course.hasMany(batch)
+center.hasMany(batch)
+season.hasMany(batch)
+
+db.sync()
